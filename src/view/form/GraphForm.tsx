6e5e2a4ea2bfe9528e27821c8b15f926fe560ded
vran <vran_dev@foxmail.com>
2024-01-06 15:55:10 +0800

    feat: use tab component to optimize form pages

--- src/view/form/GraphForm.tsx
+++ src/view/form/GraphForm.tsx
@@ -3,7 +3,6 @@ import { CodeBlockProcessor } from "src/processor/codeBlockProcessor";
 import { CellStyleRule } from "src/types";
 import { Choose } from "../choose/Choose";
 import { CellRuleItem } from "./CellRuleFormItem";
-import { Divider } from "../divider/Divider";
 import { THEMES } from "./GraphTheme";
 import { Messages } from "src/i18/messages";
 import { App } from "obsidian";
@@ -16,6 +15,7 @@ import {
 } from "./options";
 import { DataSourceFormItem } from "./DataSourceFormItem";
 import { YamlGraphConfig } from "src/processor/types";
+import { Tab, TabItem } from "../tab/Tab";
 
 export function CreateContributionGraphForm(props: {
 	yamlConfig: YamlGraphConfig;
@@ -105,282 +105,377 @@ export function CreateContributionGraphForm(props: {
 	};
 
 	return (
-		<div className="contribution-graph-modal-form">
-			<div className="form-group">
-				<Divider text={Messages.form_graph_settings.get()} />
-				<div className="form-item">
-					<span className="label">{Messages.form_title.get()}</span>
-					<div className="form-content">
-						<input
-							name="title"
-							type="text"
-							defaultValue={formData.title}
-							placeholder={Messages.form_title_placeholder.get()}
-							onChange={handleInputChange}
-							style={{
-								...formData.titleStyle,
-								fontSize: "inherits",
-								fontWeight:
-									formData.titleStyle?.fontWeight || "normal",
-								// @ts-ignore
-								textAlign:
-									formData.titleStyle?.textAlign || "left",
-							}}
-						/>
+		<>
+			<Tab
+				activeIndex={0}
+				tabs={[
+					{
+						title: Messages.form_graph_settings.get(),
+						children: (
+							<div className="contribution-graph-modal-form">
+								<div className="form-group">
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_title.get()}
+										</span>
+										<div className="form-content">
+											<input
+												name="title"
+												type="text"
+												defaultValue={formData.title}
+												placeholder={Messages.form_title_placeholder.get()}
+												onChange={handleInputChange}
+												style={{
+													...formData.titleStyle,
+													fontSize: "inherits",
+													fontWeight:
+														formData.titleStyle
+															?.fontWeight ||
+														"normal",
+													// @ts-ignore
+													textAlign:
+														formData.titleStyle
+															?.textAlign ||
+														"left",
+												}}
+											/>
 
-						<Choose
-							options={titleAlignChooseOptions}
-							defaultValue={
-								formData.titleStyle?.textAlign || "left"
-							}
-							onChoose={(option) => {
-								changeFormData("titleStyle", {
-									...formData.titleStyle,
-									textAlign: option.value,
-								});
-							}}
-						/>
-					</div>
-				</div>
+											<Choose
+												options={
+													titleAlignChooseOptions
+												}
+												defaultValue={
+													formData.titleStyle
+														?.textAlign || "left"
+												}
+												onChoose={(option) => {
+													changeFormData(
+														"titleStyle",
+														{
+															...formData.titleStyle,
+															textAlign:
+																option.value,
+														}
+													);
+												}}
+											/>
+										</div>
+									</div>
 
-				<div className="form-item">
-					<span className="label">
-						{Messages.form_graph_type.get()}
-					</span>
-					<div className="form-content">
-						<select
-							name="graphType"
-							defaultValue={
-								formData.graphType ||
-								graphOptions.find((p) => p.selected)?.value
-							}
-							onChange={handleInputChange}
-						>
-							{graphOptions.map((option) => (
-								<option value={option.value} key={option.value}>
-									{option.label}
-								</option>
-							))}
-						</select>
-					</div>
-				</div>
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_graph_type.get()}
+										</span>
+										<div className="form-content">
+											<select
+												name="graphType"
+												defaultValue={
+													formData.graphType ||
+													graphOptions.find(
+														(p) => p.selected
+													)?.value
+												}
+												onChange={handleInputChange}
+											>
+												{graphOptions.map((option) => (
+													<option
+														value={option.value}
+														key={option.value}
+													>
+														{option.label}
+													</option>
+												))}
+											</select>
+										</div>
+									</div>
 
-				<div className="form-item">
-					<span className="label">
-						{Messages.form_date_range.get()}
-					</span>
-					<div className="form-content">
-						<select
-							defaultValue={isLatestDate ? "latest" : "fixed"}
-							onChange={() => {
-								if (isLatestDate) {
-									// change to fixed date should clear days field
-									changeFormData("days", undefined);
-								}
-								setIsLatestDate(!isLatestDate);
-							}}
-						>
-							<option value="fixed">
-								{Messages.form_date_range_fixed_date.get()}
-							</option>
-							<option value="latest">
-								{Messages.form_date_range_latest_days.get()}
-							</option>
-						</select>
-					</div>
-				</div>
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_date_range.get()}
+										</span>
+										<div className="form-content">
+											<select
+												defaultValue={
+													isLatestDate
+														? "latest"
+														: "fixed"
+												}
+												onChange={() => {
+													if (isLatestDate) {
+														// change to fixed date should clear days field
+														changeFormData(
+															"days",
+															undefined
+														);
+													}
+													setIsLatestDate(
+														!isLatestDate
+													);
+												}}
+											>
+												<option value="fixed">
+													{Messages.form_date_range_fixed_date.get()}
+												</option>
+												<option value="latest">
+													{Messages.form_date_range_latest_days.get()}
+												</option>
+											</select>
+										</div>
+									</div>
 
-				<div className="form-item">
-					<span className="label"></span>
-					<div className="form-content">
-						{isLatestDate ? (
-							<>
-								<input
-									id="days"
-									name="days"
-									type="number"
-									defaultValue={formData.days}
-									min={1}
-									placeholder={Messages.form_date_range_latest_days_placeholder.get()}
-									onChange={(e) =>
-										changeFormData(
-											"days",
-											parseInt(e.target.value)
-										)
-									}
-								/>
-							</>
-						) : (
-							<>
-								<input
-									id="fromDate"
-									name="fromDate"
-									type="date"
-									defaultValue={formData.fromDate}
-									placeholder="from date, such as 2023-01-01"
-									onChange={handleInputChange}
-								/>
-								&nbsp;-&nbsp;
-								<input
-									id="toDate"
-									name="toDate"
-									type="date"
-									defaultValue={formData.toDate}
-									placeholder="to date, such as 2023-12-31"
-									onChange={handleInputChange}
-								/>
-							</>
-						)}
-					</div>
-				</div>
+									<div className="form-item">
+										<span className="label"></span>
+										<div className="form-content">
+											{isLatestDate ? (
+												<>
+													<input
+														id="days"
+														name="days"
+														type="number"
+														defaultValue={
+															formData.days
+														}
+														min={1}
+														placeholder={Messages.form_date_range_latest_days_placeholder.get()}
+														onChange={(e) =>
+															changeFormData(
+																"days",
+																parseInt(
+																	e.target
+																		.value
+																)
+															)
+														}
+													/>
+												</>
+											) : (
+												<>
+													<input
+														id="fromDate"
+														name="fromDate"
+														type="date"
+														defaultValue={
+															formData.fromDate
+														}
+														placeholder="from date, such as 2023-01-01"
+														onChange={
+															handleInputChange
+														}
+													/>
+													&nbsp;-&nbsp;
+													<input
+														id="toDate"
+														name="toDate"
+														type="date"
+														defaultValue={
+															formData.toDate
+														}
+														placeholder="to date, such as 2023-12-31"
+														onChange={
+															handleInputChange
+														}
+													/>
+												</>
+											)}
+										</div>
+									</div>
 
-				{formData.graphType == "month-track" ? null : (
-					<div className="form-item">
-						<span className="label">
-							{Messages.form_start_of_week.get()}
-						</span>
-						<div className="form-content">
-							<select
-								id="startOfWeek"
-								name="startOfWeek"
-								defaultValue={
-									formData.startOfWeek != undefined
-										? formData.startOfWeek
-										: startOfWeekOptions.find(
-												(p) => p.selected
-										)?.value
-								}
-								onChange={handleInputChange}
-							>
-								{startOfWeekOptions.map((option) => (
-									<option
-										value={option.value}
-										key={option.value}
-									>
-										{option.label}
-									</option>
-								))}
-							</select>
-						</div>
-					</div>
-				)}
-
-				<DataSourceFormItem
-					dataSource={formData.dataSource}
-					onChange={(newDataSource) => {
-						changeFormData("dataSource", newDataSource);
-					}}
-					app={props.app}
-				/>
-			</div>
-
-			<div className="form-group">
-				<Divider text={Messages.form_style_settings.get()} />
+									<DataSourceFormItem
+										dataSource={formData.dataSource}
+										onChange={(newDataSource) => {
+											changeFormData(
+												"dataSource",
+												newDataSource
+											);
+										}}
+										app={props.app}
+									/>
+								</div>
+							</div>
+						),
+					},
+					{
+						title: Messages.form_style_settings.get(),
+						children: (
+							<div className="contribution-graph-modal-form">
+								<div className="form-group">
+									{formData.graphType ==
+									"month-track" ? null : (
+										<div className="form-item">
+											<span className="label">
+												{Messages.form_start_of_week.get()}
+											</span>
+											<div className="form-content">
+												<select
+													id="startOfWeek"
+													name="startOfWeek"
+													defaultValue={
+														formData.startOfWeek !=
+														undefined
+															? formData.startOfWeek
+															: startOfWeekOptions.find(
+																	(p) =>
+																		p.selected
+															)?.value
+													}
+													onChange={handleInputChange}
+												>
+													{startOfWeekOptions.map(
+														(option) => (
+															<option
+																value={
+																	option.value
+																}
+																key={
+																	option.value
+																}
+															>
+																{option.label}
+															</option>
+														)
+													)}
+												</select>
+											</div>
+										</div>
+									)}
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_show_cell_indicators.get()}
+										</span>
+										<div className="form-content">
+											<input
+												name="showCellRuleIndicators"
+												type="checkbox"
+												className="checkbox"
+												defaultChecked={
+													formData.showCellRuleIndicators
+												}
+												onChange={() =>
+													changeFormData(
+														"showCellRuleIndicators",
+														!formData.showCellRuleIndicators
+													)
+												}
+											/>
+										</div>
+									</div>
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_cell_shape.get()}
+										</span>
+										<div className="form-content">
+											<select
+												name="cellShape"
+												defaultValue={getDefaultCellShape()}
+												onChange={handleCellShapeChange}
+											>
+												{cellShapes.map((option) => (
+													<option
+														value={option.value}
+														key={option.label}
+													>
+														{option.label}
+													</option>
+												))}
+											</select>
+										</div>
+									</div>
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_theme.get()}
+										</span>
+										<div className="form-content">
+											<select
+												name="theme"
+												aria-placeholder="select theme to generate style"
+												onChange={handleThemeChanged}
+											>
+												{THEMES.map((option) => (
+													<option
+														value={option.name}
+														key={option.name}
+													>
+														{option.name}
+													</option>
+												))}
+											</select>
+										</div>
+									</div>
+									<div className="form-item">
+										<span className="label">
+											{Messages.form_cell_style_rules.get()}
+										</span>
+										<div className="form-vertical-content">
+											{cellRules.map((rule) => {
+												return (
+													<CellRuleItem
+														rule={rule}
+														key={rule.id}
+														onChange={(newRule) => {
+															const newRules =
+																cellRules.map(
+																	(r) => {
+																		if (
+																			r.id ==
+																			newRule.id
+																		) {
+																			return newRule;
+																		} else {
+																			return r;
+																		}
+																	}
+																);
+															setCellRules(
+																newRules
+															);
+														}}
+														onRemove={(
+															id: string
+														) => {
+															const newRules =
+																cellRules.filter(
+																	(r) =>
+																		r.id !=
+																		id
+																);
+															setCellRules(
+																newRules
+															);
+														}}
+													/>
+												);
+											})}
+											<button
+												onClick={() => addCellRule()}
+												className="cell-rule-add-button"
+											>
+												+
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						),
+					},
+				]}
+			></Tab>
+			<div className="contribution-graph-modal-form">
+				<div
+					className="preview-container"
+					ref={previewContainerRef}
+				></div>
 				<div className="form-item">
-					<span className="label">
-						{Messages.form_show_cell_indicators.get()}
-					</span>
 					<div className="form-content">
-						<input
-							name="showCellRuleIndicators"
-							type="checkbox"
-							className="checkbox"
-							defaultChecked={formData.showCellRuleIndicators}
-							onChange={() =>
-								changeFormData(
-									"showCellRuleIndicators",
-									!formData.showCellRuleIndicators
-								)
-							}
-						/>
-					</div>
-				</div>
-				<div className="form-item">
-					<span className="label">
-						{Messages.form_cell_shape.get()}
-					</span>
-					<div className="form-content">
-						<select
-							name="cellShape"
-							defaultValue={getDefaultCellShape()}
-							onChange={handleCellShapeChange}
-						>
-							{cellShapes.map((option) => (
-								<option value={option.value} key={option.label}>
-									{option.label}
-								</option>
-							))}
-						</select>
-					</div>
-				</div>
-				<div className="form-item">
-					<span className="label">{Messages.form_theme.get()}</span>
-					<div className="form-content">
-						<select
-							name="theme"
-							aria-placeholder="select theme to generate style"
-							onChange={handleThemeChanged}
-						>
-							{THEMES.map((option) => (
-								<option value={option.name} key={option.name}>
-									{option.name}
-								</option>
-							))}
-						</select>
-					</div>
-				</div>
-				<div className="form-item">
-					<span className="label">
-						{Messages.form_cell_style_rules.get()}
-					</span>
-					<div className="form-vertical-content">
-						{cellRules.map((rule) => {
-							return (
-								<CellRuleItem
-									rule={rule}
-									key={rule.id}
-									onChange={(newRule) => {
-										const newRules = cellRules.map((r) => {
-											if (r.id == newRule.id) {
-												return newRule;
-											} else {
-												return r;
-											}
-										});
-										setCellRules(newRules);
-									}}
-									onRemove={(id: string) => {
-										const newRules = cellRules.filter(
-											(r) => r.id != id
-										);
-										setCellRules(newRules);
-									}}
-								/>
-							);
-						})}
-						<button
-							onClick={() => addCellRule()}
-							className="cell-rule-add-button"
-						>
-							+
+						<button className="button" onClick={onPreview}>
+							{Messages.form_button_preview.get()}
+						</button>
+						<button className="button" onClick={onSubmit}>
+							{Messages.form_button_save.get()}
 						</button>
 					</div>
 				</div>
 			</div>
-
-			<div className="form-item">
-				<div className="form-content">
-					<button className="button" onClick={onPreview}>
-						{Messages.form_button_preview.get()}
-					</button>
-					<button className="button" onClick={onSubmit}>
-						{Messages.form_button_save.get()}
-					</button>
-				</div>
-			</div>
-
-			<div className="preview-container" ref={previewContainerRef}></div>
-		</div>
+		</>
 	);
 }
 
